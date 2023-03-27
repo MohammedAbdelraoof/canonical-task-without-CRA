a95b5666e16df2ecedd81b868b24f7f31ea5637d
MohammedAbdelraoof <Mohammed.abdelraoof@syngenta.com>
2023-03-27 04:14:20 +0200

    app  start

--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,32 @@ module.exports = {
     ],
 
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react']
-                        }
+                    }
                 }
             },
             {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: ['ts-loader']
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     },
     resolve: {
